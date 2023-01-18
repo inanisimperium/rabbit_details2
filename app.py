57c831c0eab102040614143d3728436d97d664fc
inanisimperium <122769153+inanisimperium@users.noreply.github.com>
2023-01-18 03:00:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('rabbit_model.pkl', 'rb'))
 
 @app.route("/")
 def hello():
@@ -10,11 +10,11 @@ def hello():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    rooms = int(request.form['rooms'])
-    distance = int(request.form['distance'])
-    prediction = model.predict([[rooms, distance]])
+    height = int(request.form['height'])
+    width = int(request.form['width'])
+    prediction = model.predict([[height, width]])
     output = round(prediction[0], 2)
-    return render_template('index.html', prediction_text=f'A house with {rooms} rooms per dwelling and located {distance} km to employment centers has a value of ${output}K')
+    return render_template('index.html', prediction_text=f'A rabbit with {height} height and {wdith} width has a value of {output}kg')
 
 if __name__ == "__main__":
     app.run()
