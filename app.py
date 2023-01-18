1dfd3443ca2c0f4239d298e730f8ed76fd099f3e
inanisimperium <122769153+inanisimperium@users.noreply.github.com>
2023-01-18 03:36:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def predict():
     width = int(request.form['width'])
     prediction = model.predict([[height, width]])
     output = round(prediction[0], 2)
-    return render_template('index.html', prediction_text=f'A rabbit with {height} height and {width} width has a value of {output}kg')
+    return render_template('index.html', prediction_text=f'A rabbit with {height} height and {width} width has a value of {output} kg')
 
 if __name__ == "__main__":
     app.run()
